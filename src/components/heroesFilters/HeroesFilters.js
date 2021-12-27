10ba008569210d4041ac46b4cac7e1f8a115d798
unknown <C:\Users\Metrolog\AppData\Roaming\The Bat!>
2021-12-27 11:58:19 +0300

    Redux Toolkit Refactoring with Redux Toolkit

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -1,31 +1,23 @@
-
-// Задача для этого компонента:
-// Фильтры должны формироваться на основании загруженных данных
-// Фильтры должны отображать только нужных героев при выборе
-// Активный фильтр имеет класс active
-// Изменять json-файл для удобства МОЖНО!
-// Представьте, что вы попросили бэкенд-разработчика об этом
-
 import { useHttp } from "../../hooks/http.hook";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import classNames from "classnames";
+import store from '../../store';
 
-import { filtersFetching, filtersFetched, filtersFetchingError, activeFilterChanged } from "../../actions";
+import { filtersChanged, fetchFilters, selectAll } from './filtersSlice';
 
 import Spinner from '../spinner/Spinner'
 
 
 const HeroesFilters = () => {
-    const {filters, filtersLoadingStatus, activeFilter} = useSelector(state => state);
+    const {filtersLoadingStatus, activeFilter} = useSelector(state => state.filters);
+    const filters = selectAll(store.getState());
     const dispatch = useDispatch();
     const {request} = useHttp();
 
     useEffect(() => {
-        dispatch(filtersFetching());
-        request("http://localhost:3001/filters")
-            .then(data => dispatch(filtersFetched(data)))
-            .catch(() => dispatch(filtersFetchingError()))
+        dispatch(fetchFilters(request));
+
         // eslint-disable-next-line
     }, []);
 
@@ -47,7 +39,7 @@ const HeroesFilters = () => {
                         key={name}
                         id={name}
                         className={btnClass}
-                        onClick={() => dispatch(activeFilterChanged(name))}>{label}</button>
+                        onClick={() => dispatch(filtersChanged (name))}>{label}</button>
                     
         });
 
