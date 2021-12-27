db7221421e4141a60963265e7dd57626d363fb41
unknown <C:\Users\Metrolog\AppData\Roaming\The Bat!>
2021-12-27 15:27:16 +0300

    RTK Query refactoring

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,12 +1,8 @@
-import {useHttp} from '../../hooks/http.hook';
-import { useEffect, useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+
+import { useCallback, useMemo } from 'react';
+import { useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-
-
-
-import { heroDeleted, fetchHeroes, filteredHeroesSelector } from './heroesSlice';
-
+import { useGetHeroesQuery, useDeleteHeroMutation } from '../../api/apiSlice';
 
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
@@ -17,39 +13,39 @@ import './heroesList.scss';
 
 const HeroesList = () => {
 
+    const {
+        data: heroes = [],
+        isLoading,
+        isError,
+    } = useGetHeroesQuery();
 
+    const [deleteHero] = useDeleteHeroMutation();
 
-/*     const filteredHeroes = useSelector(state => {
-        if (state.filters.activeFilter === 'all') {
-            return state.heroes.heroes;
+    const activeFilter = useSelector(state => state.filters.activeFilter);
+
+    const filteredHeroes = useMemo(() => {
+        const filteredHeroes = heroes.slice();
+        
+        if (activeFilter === 'all') {
+            return filteredHeroes;
         } else {
-            return state.heroes.heroes.filter(item => item.element === state.filters.activeFilter)
+            return filteredHeroes.filter(item => item.element === activeFilter);
         }
-    }) */
-    const filteredHeroes =useSelector(filteredHeroesSelector);
 
-    const heroesLoadingStatus = useSelector(state => state.heroes.heroesLoadingStatus);
-    const dispatch = useDispatch();
-    const {request} = useHttp();
-
-    useEffect(() => {
-        dispatch(fetchHeroes());
+    }, [heroes, activeFilter]);
 
 
-        // eslint-disable-next-line
-    }, []);
+
+
 
     const onDelete = useCallback((id) => {
-        request(`http://localhost:3001/heroes/${id}`, "DELETE")
-            .then(data => console.log(data, 'Deleted'))
-            .then(dispatch(heroDeleted(id)))
-            .catch(err => console.log(err));
+        deleteHero(id);
         // eslint-disable-next-line
-    },[request]);
+    },[]);
 
-    if (heroesLoadingStatus === "loading") {
+    if (isLoading) {
         return <Spinner/>;
-    } else if (heroesLoadingStatus === "error") {
+    } else if (isError) {
         return <h5 className="text-center mt-5">Loading error</h5>
     }
 
